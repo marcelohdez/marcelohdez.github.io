db75a6e9a8b40604d1ae26c9b382ec8cef88ca34
Marcelo Hernandez Lopez <marcelohdez.inq@gmail.com>
2024-05-01 22:02:00 -0400

    begin redesign with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Fira Sans", "sans-serif", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
