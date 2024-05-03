2a6d43e1e066754d329366d32d2def4a30141349
Marcelo Hernandez Lopez <marcelohdez.inq@gmail.com>
2024-05-03 16:38:43 -0400

    theme: classify lists into cards and add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
       sans: ["Fira Sans", "sans-serif"],
       mono: ["Fira Code", "monospace"],
     },
-    extend: {},
+    extend: {
+      animation: {
+        fadein: "fadein 1s ease-in-out",
+      },
+      keyframes: {
+        fadein: {
+          from: { transform: "translateY(2rem)", opacity: 0 },
+          to: { transform: "translateY(0)", opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 };
